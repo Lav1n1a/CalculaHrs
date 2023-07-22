37b19dea7e30eed8052ab971c95fa82963b16aef
Lavínia de Campos <106318260+Lav1n1a@users.noreply.github.com>
2023-07-22 08:29:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,129 @@
-*{
-    margin: 0;
-    padding: 0;
-    } 
+function addOperacao(operacao){
+    document.getElementById('operacao').value = operacao;
+    document.getElementById('operacao').value = operacao;
+
+    let valor1 = document.getElementById('valor1').value;
     
-    .fundo{
-    background-color: #046E60;
-    height: 100vh;
-    color: white;
-    font-family: Arial, Helvetica, sans-serif;
-    }
-    
-    .calculadora{
-    position: absolute;
-    background-color:#159C8C;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    border-radius: 15px;
-    padding: 15px;
-    }
-    
-    .botao{
-    width: 50px;
-    height: 50px;
-    font-size: 25px;
-    cursor: pointer;
-    margin: 3px;
-    background-color: rgb(31, 31, 31);
-    border: none;
-    color: white;
-    }
-    
-    hr {
-        margin: 5px;
-    }
-    
-    .botao2 {
-        background-color: #136961a6;
-        border: none;
-        border-radius: 5px;
-        color: white;
-        width: 85px;
-        height: 80px;
-        margin: 10px 5px;
-        padding: 5px;
-        font-size: 14px;
-        cursor: pointer;
-    }
-    
-    .botao:hover{
-    background-color: black;
-    }
-    
-    #resultado{
-    width: 272px;
-    background-color: white;
-    height: 30px;
-    margin: 5px;
-    font-size: 25px;
-    color: black;
-    text-align: right;
-    padding: 5px;
-    }
-    .rodape-historico {
-        margin: 10px;
+    document.getElementById('resultado').value = mask(valor1) + operacao;
+}
+
+function adicionar(num) {
+    let resultado = document.getElementById('resultado').value;
+
+    let primeiroValor = document.getElementById('valor1').value;
+    let segundoValor = document.getElementById('valor2').value;
+    let operacao = document.getElementById('operacao').value;
+
+    if (operacao == '+' || operacao == '-') {
+        
+        resultado = mask(primeiroValor) + operacao + mask(segundoValor + num);
+        document.getElementById('valor2').value = segundoValor + num
+
+    } else if (operacao == '*' || operacao == '/') {
+        resultado = mask(primeiroValor) + operacao + (segundoValor + num);
+        
+        document.getElementById('valor2').value = segundoValor + num;
+    } else {
+        resultado = mask(primeiroValor + num);
+        document.getElementById('valor1').value = primeiroValor + num;
     }
 
-    .content-historico {
-        display: flex;
-        justify-content: space-between;
-    }
+    document.getElementById('resultado').value = resultado;
+}
+
+function limpar() {
+    document.getElementById('resultado').value = null;
+    document.getElementById('valor1').value = null;
+    document.getElementById('valor2').value = null;
+    document.getElementById('operacao').value = null;
+}
+
+function voltar() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function apagarHistorico() {
+    let his = document.getElementById('list');
+    his.innerHTML = null;
+}
+
+function convertToTime(minutes) {
+    let hours = Math.floor(minutes / 60);
+    let mins = minutes % 60;
+    let formattedHours = hours < 10 ? '0' + hours : hours;
+    let formattedMinutes = mins < 10 ? '0' + mins : mins;
+    return formattedHours + ':' + formattedMinutes;
+}
+
+function calcular() {
+    let primeiroValor = document.getElementById('valor1').value;
+    let segundoValor = document.getElementById('valor2').value;
+    let operacao = document.getElementById('operacao').value;
     
-    .exclui-historico {
-        background-color: #159C8C;
-        border: none;
-        padding: 5px;
-        width: 30px;
-        color: white;
-        font-size: 15px;
-        cursor: pointer;
+    let historico = document.getElementById('list');//Variável para acrescentar histórico das operações
+    let itensHistorico = document.createElement('li');
+
+    let resultado;
+
+    let array1 = mask(primeiroValor).split(':');//Separa os valores através do split, por exemplo 10:20 para 10,20
+    let array2 = mask(segundoValor).split(':');
+    var tempo1 = parseInt(array1[0] * 60) + parseInt(array1[1]);//divide horas para 60 e só repete o minuto
+    var tempo2 = parseInt(array2[0] * 60) + parseInt(array2[1]);
+
+    if (operacao == '+') {
+        let result = tempo1 + tempo2;
+        resultado = convertToTime(result);
+    } else if (operacao == '-') {
+        let result = tempo1 - tempo2;
+        resultado = convertToTime(result);
+    } else if (operacao == '*') {
+        let result = tempo1 * tempo2;
+        resultado = convertToTime(result);
+    } else if (operacao == '/') {
+        let result = tempo1 / tempo2;
+        resultado = convertToTime(result);
+    } else {
+        resultado = 'Operação inválida';
     }
-    .exclui-historico:hover {
-        background-color: white;
-        color: #159C8C;;
+
+    document.getElementById('resultado').value = resultado;
+
+    //itensHistorico.classList.add('hist-resultado');
+    
+    itensHistorico.textContent = `${mask(primeiroValor)} + ${mask(segundoValor)} = ${resultado}`;
+    historico.appendChild(itensHistorico);
+
+
+}
+
+function mask(numero) {
+    numero = numero.toString().replace(':', '');
+    if(numero.length == 1){
+        numero = "00:0" + numero;
+    }else if(numero.length == 2){
+        numero = "00:" + numero.slice(0, 2);
+    } else if (numero.length == 3) {
+        numero = "0" + numero.slice(0, 1) + ":" + numero.slice(-2);
+    } else if (numero.length > 3) {
+        numero = numero.slice(0, numero.length - 2) + ":" + numero.slice(-2);
     }
-    #list{
-        margin-top: 10px;
-    }
-    li {
-        text-decoration: none;
-        list-style: none;
-    }
+
+    return numero;
+}
+
+function diasTrabalhados(){
+    let horasLista = document.getElementById('list').innerHTML;
+
+    let filhos = horasLista.children;
+    let separarValores = filhos.split('=');
+    let valoreSeparados = parseInt(separarValores[1] / 24);
+    console.log(valoreSeparados);
+
+    let mostrarDiasFormatado = parseInt(separarValores[0]) + ":" + valoreSeparados;
+
+    document.getElementById('list').innerHTML = mostrarDiasFormatado;
+
+}
+
 
